68a56436ffedd51928a9bb05595b2e634cb652ca
Schizo <yafes7@yahoo.de>
2015-04-06 01:54:03 +0200

    Added minimal exoplayer view

--- app/src/main/java/fmx/kssgcm/MainActivity.java
+++ app/src/main/java/fmx/kssgcm/MainActivity.java
@@ -2,6 +2,7 @@ package fmx.kssgcm;
 
 
 import android.content.Intent;
+import android.media.MediaCodec;
 import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -9,27 +10,60 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.SurfaceHolder;
 
+
+
 import com.google.android.exoplayer.ExoPlayer;
+import com.google.android.exoplayer.MediaCodecAudioTrackRenderer;
+import com.google.android.exoplayer.MediaCodecVideoTrackRenderer;
+import com.google.android.exoplayer.SampleSource;
+import com.google.android.exoplayer.TrackRenderer;
+import com.google.android.exoplayer.VideoSurfaceView;
+import com.google.android.exoplayer.source.DefaultSampleSource;
+import com.google.android.exoplayer.source.FrameworkSampleExtractor;
 
 public class MainActivity extends ActionBarActivity  {
 
+    private VideoSurfaceView surfaceView;
 
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        surfaceView = (VideoSurfaceView)findViewById(R.id.surface_view);
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
         new GcmRegistrationAsyncTask(this).execute();
 
-        //player = ExoPlayer.Factory.newInstance(RENDERER_COUNT);
 
-       /* Intent i = new Intent(Intent.ACTION_VIEW);
-        i.setPackage("com.mxtech.videoplayer.ad");
-        i.setDataAndType(Uri.parse("http://www.example.com"), "video/h264");
-        startActivity(i);*/
+
+
+        Uri uri = Uri.parse("http://html5demos.com/assets/dizzy.mp4");
+        final int numRenderers = 2;
+
+
+        // Build the sample source
+        DefaultSampleSource sampleSource =
+                new DefaultSampleSource(new FrameworkSampleExtractor(this, uri, null), 2);
+
+        // Build the track renderers
+        TrackRenderer videoRenderer = new MediaCodecVideoTrackRenderer(sampleSource, MediaCodec.VIDEO_SCALING_MODE_SCALE_TO_FIT);
+        TrackRenderer audioRenderer = new MediaCodecAudioTrackRenderer(sampleSource);
+
+        // Build the ExoPlayer and start playback
+        ExoPlayer exoPlayer = ExoPlayer.Factory.newInstance(numRenderers);
+        exoPlayer.prepare(videoRenderer, audioRenderer);
+
+        // Pass the surface to the video renderer.
+        exoPlayer.sendMessage(videoRenderer, MediaCodecVideoTrackRenderer.MSG_SET_SURFACE, surfaceView);
+
+        exoPlayer.setPlayWhenReady(true);
+
+
     }
 
 
---
+++ app/src/main/java/fmx/kssgcm/VodPlayer.java
@@ -0,0 +1,38 @@
+package fmx.kssgcm;
+
+import android.app.Activity;
+import android.media.MediaCodec;
+import android.net.Uri;
+import android.view.Surface;
+
+import com.google.android.exoplayer.ExoPlayer;
+import com.google.android.exoplayer.MediaCodecAudioTrackRenderer;
+import com.google.android.exoplayer.MediaCodecVideoTrackRenderer;
+import com.google.android.exoplayer.SampleSource;
+import com.google.android.exoplayer.TrackRenderer;
+
+import com.google.android.exoplayer.*;
+
+import android.content.Context;
+import android.media.MediaCodec;
+import android.net.Uri;
+
+/**
+ * Created by fmx on 06.04.15.
+ */
+public class VodPlayer   {
+
+
+
+
+
+    void play(String strUri, Surface surface) {
+
+    }
+
+    void release() {
+        // Don't forget to release when done!
+//        exoPlayer.release();
+    }
+
+}
